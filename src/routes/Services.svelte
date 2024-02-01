5a10ed395bb0bb91111f6340709ec873330ae6c7
Ayoub <ayoub.fiadh@gmail.com>
2024-01-31 21:50:41 -0500

    Init swiper js

--- src/routes/Services.svelte
+++ src/routes/Services.svelte
@@ -1,38 +1,48 @@
 <script>
-	import Carousel from 'svelte-carousel';
-	import { browser } from '$app/environment';
-  import img1 from '$lib/images/item1.jpg';
-  import img2 from '$lib/images/item2.jpg';
-  import img3 from '$lib/images/item3.jpg';
-  import img4 from '$lib/images/item4.jpg';
+  import { register } from "swiper/element/bundle";
+  import { browser } from "$app/environment";
+  import img1 from "$lib/images/item1.jpg";
+  import img2 from "$lib/images/item2.jpg";
+  import img3 from "$lib/images/item3.jpg";
+  import img4 from "$lib/images/item4.jpg";
+  import { onMount } from "svelte";
 
   let items = [
-    {title: "title 1", img: img1},
-    {title: "title 2", img: img2},
-    {title: "title 3", img: img3},
-    {title: "title 4", img: img4},
-  ]
-  let carousel;
-
+    { title: "title 1", img: img1 },
+    { title: "title 2", img: img2 },
+    { title: "title 3", img: img3 },
+    { title: "title 4", img: img4 },
+    { title: "title 1", img: img1 },
+    { title: "title 2", img: img2 },
+    { title: "title 3", img: img3 },
+    { title: "title 4", img: img4 }
+  ];
+  onMount(()=> {
+    register();
+  })
 </script>
+
 <section class="main-content h-[80vh]">
   <div class="show-case py-8 px-[calc(5%+20px)] flex justify-center">
-    {#if browser}
-      <Carousel
-        bind:this={carousel}
-        particlesToShow={3}
-        infinite={false}
-      >
+    <swiper-container
+      slides-per-view={"auto"}
+      css-mode={true}
+    >
       {#each items as item}
-        <div class="service-card mx-4 flex flex-col relative overflow-hidden rounded-lg max-w-[150px] w-[150px] md:w-[200px] h-[300px] text-white hover:text-primary transition-colors group">
-          <img src={item.img} alt={item.title} class="z-0 w-full h-full object-cover transform transition-transform delay-300 group-hover:scale-[1.3]" />
-          <div class="card-info">
-            <h2 class="font-bold text-2xl">{item.title}</h2>
-          </div>
-        </div>
+        <swiper-slide
+            class="service-card mx-4 flex flex-col relative overflow-hidden rounded-lg w-[150px] md:w-[300px] h-[300px] text-white hover:text-primary transition-colors group"
+          >
+            <img
+              src={item.img}
+              alt={item.title}
+              class="z-0 w-full h-full object-cover transform transition-transform delay-300 group-hover:scale-[1.3]"
+            />
+            <div class="card-info">
+              <h2 class="font-bold text-2xl">{item.title}</h2>
+            </div>
+        </swiper-slide>
       {/each}
-    </Carousel>
-    {/if}
+    </swiper-container>
   </div>
 </section>
 
@@ -50,12 +60,13 @@
     @apply content-[""] absolute bottom-0 right-0 w-full h-full transition-all duration-1000 ease-in-out z-10;
   }
   .service-card::before {
-    @apply  bg-gradient-top;
+    @apply bg-gradient-top;
   }
   .service-card::after {
-    @apply  bg-gradient-bottom;
+    @apply bg-gradient-bottom;
   }
-  .service-card:hover::before, .service-card:hover::after {
+  .service-card:hover::before,
+  .service-card:hover::after {
     @apply h-0 w-0;
   }
 </style>
