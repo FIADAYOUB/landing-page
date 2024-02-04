18ea3c1bdde222fcfdcc4dbd84fcb36178fa30bc
Ayoub <ayoub.fiadh@gmail.com>
2024-02-03 22:02:14 -0500

    Uninstall particles js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,9 @@
 <script>
-  import { particlesConfig } from '$lib/constants/particles'
   import Services from './Services.svelte';
-	import '$lib/constants/particles';
-	import Particles, { particlesInit } from '@tsparticles/svelte';
-  //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-  import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 
-  let onParticlesLoaded = (event) => {
-      const particlesContainer = event.detail.particles;
-
-      // you can use particlesContainer to call all the Container class
-      // (from the core library) methods like play, pause, refresh, start, stop
-  };
-
-  void particlesInit(async (engine) => {
-      // call this once per app
-      // you can use main to customize the tsParticles instance adding presets or custom shapes
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadFull(engine);
-      await loadSlim(engine);
-  });
 </script>
 <div class="page-index">
 	<div class="page-content">
-		<Particles
-			id="tsparticles"
-			class="put your classes here"
-			style=""
-			options="{particlesConfig}"
-			on:particlesLoaded="{onParticlesLoaded}"
-			/>
 		<section class="index-header h-[80vh] relative py-2 px-[5%] bg-secondary-base text-white" id="particles-js">
 			<div class="headline-container h-full w-full flex flex-1 flex-col justify-center items-center max-w-[400px] mx-auto">
 				<h1 class="text-[3em] font-bold text-center mb-3 font-poppins">WE ARE A LANDING PAGE</h1>
