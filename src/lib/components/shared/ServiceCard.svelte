5118e57b93b59972146c8ec7de97e9d67bda778f
Ayoub <ayoub.fiadh@gmail.com>
2024-02-04 22:22:59 -0500

    Maj css

--- src/lib/components/shared/ServiceCard.svelte
+++ src/lib/components/shared/ServiceCard.svelte
@@ -3,8 +3,8 @@
   const {title, description, icon} = service;
 </script>
 
-<div class="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg shadow-box-shadow relative overflow-hidden">
-    <div class="rounded-xl bg-gray-300 dark:bg-gray-950 p-3 text-heading-1 w-max relative">
+<div class="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg shadow-box-shadow relative overflow-hidden transition-all duration-300 ease-in hover:scale-105">
+    <div class="rounded-xl bg-tertiary-base dark:bg-gray-950 p-3 text-heading-1 w-max relative">
        {@html icon}
     </div>
     <div class="mt-6 space-y-4 relative">
@@ -15,5 +15,5 @@
             {description}
         </p>
     </div>
-    <span class="absolute w-32 aspect-square -bottom-16 -right-16 bg-primary/10 rounded-full"></span>
+    <span class="absolute w-32 aspect-square -bottom-16 -right-16 bg-tertiary-base rounded-full"></span>
 </div>
