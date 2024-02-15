264368873bc07334641a873985e8d21211924717
Ayoub <ayoub.fiadh@gmail.com>
2024-02-14 22:31:53 -0500

    Add flip back descritopn

--- src/lib/components/shared/ServiceCard.svelte
+++ src/lib/components/shared/ServiceCard.svelte
@@ -1,21 +1,34 @@
 <script>
   export let service;
-  const { title, description, icon } = service;
+
   import img1 from "$lib/images/item3.jpg";
+
+  const { title, description, icon } = service;
+
+  let showDescription = false;
 </script>
 
 <div
   class="service_card min-h-[480px] h-[480px] w-[380px] max-w-[380px] relative rounded-[20px] bg-[rgba(255,255,255,.1)] p-[5px] border-[1px] border-gray-800"
 >
-  <div
-    class="service_card-front bg-[#090121] rotate-0 flex flex-col h-full rounded-[20px] border-[1px] border-gray-800"
-  >
-    <div class="service_card-image">
-      <img src={img1} alt="" class="h-full w-full bg-cover object-cover" />
+  <div class="flip-card-inner" class:flip-it={showDescription}>
+    <div
+      class="service_card-front bg-[#090121] rotate-0 flex flex-col w-full h-full rounded-[20px] border-[1px] border-gray-800"
+    >
+      <div class="service_card-image">
+        <img src={img1} alt="" class="h-full w-full bg-cover object-cover" />
+      </div>
+      <div class="service__card-content">
+        <div class="service_card-title text-stroke">
+          {title}
+        </div>
+      </div>
     </div>
-    <div class="service__card-content">
-      <div class="service_card-title text-stroke">
-        {title}
+    <div class="flip-card-back conceal-answer">
+      <div>
+        <p>
+          {description}
+        </p>
       </div>
     </div>
   </div>
@@ -23,9 +36,18 @@
 
 <style lang="postcss">
   .service_card-image {
-    @apply absolute top-0 left-0 right-0 bottom-0 opacity-50 overflow-hidden rounded-[20px] grayscale-[50%] transition-all duration-500 ease-in-out
+    @apply absolute top-0 left-0 right-0 bottom-0 opacity-50 overflow-hidden rounded-[20px] grayscale-[50%] transition-all duration-500 ease-in-out;
   }
-
+  .flip-it {
+    transform: rotateY(180deg);
+  }
+  .flip-card-inner {
+    @apply relative w-full h-full text-center flex items-center justify-center bg-black rounded-[20px];
+  }
+  .flip-card-inner {
+    transition: transform 0.4s;
+    transform-style: preserve-3d;
+  }
   .service_card:hover .service_card-image {
     @apply opacity-100 grayscale-0;
   }
@@ -41,5 +63,15 @@
   }
   .service_card-title {
     @apply flex items-center justify-center text-center font-paytone font-medium text-[2em];
+  }
+  .flip-card-back {
+    @apply absolute w-full h-full flex items-center justify-center bg-black p-6 rounded-[20px];
+  }
+  .flip-card-back {
+    backface-visibility: hidden;
+    transform: rotateY(180deg);
+  }
+  .conceal-answer {
+    animation: revealTextSlowly 0.3s forwards;
   }
 </style>
